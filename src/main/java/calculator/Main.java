bd03ac6d3cbdb2cdbdfbcd65afc35914e6b39063
Hasanabbas-Momin <Hasan440m@gmail.com>
2023-11-23 17:16:45 +0530

    ngrok check

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+// ngrok check
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args){
